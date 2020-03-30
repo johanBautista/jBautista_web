4c8ff54994c3bf9bb2b47354e3e3ebb38699bece
johan Bautista <jsbparra@gmail.com>
2020-03-30 03:39:05 +0200

    add css home-page

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import './Home.css'
+import './Home.css';
 
 const Home = () => {
   return (
     <div className="img-home">
-
       <div className="text-center container">
         <h1 className="title-home">Full Stack Developer</h1>
-        <hr />
+
+        <div className="line-style"></div>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
           quia, cupiditate ullam asperiores officia
