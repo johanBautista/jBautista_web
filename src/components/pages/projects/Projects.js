9fed1f6533406bfb0788926d81788e9711d999c6
johan Bautista <jsbparra@gmail.com>
2020-04-21 15:37:50 +0200

    wip projects

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -2,174 +2,95 @@
 import React from 'react';
 import { Card, CardColumns } from 'react-bootstrap';
 import './Project.css';
-import aha from '../../img/ah-1.png';
-import lucky from '../../img/lk-1.png';
-import cita from '../../img/vt-1.png';
-import etiqueta from '../../img/et-1.png';
-import cafe from '../../img/cc-1.png';
+
 import okey from '../../img/okey.png';
-import pt2 from '../../img/pt2.png';
+
+const info = [
+  {
+    id: '1',
+    titulo: 'Ok To Play',
+    description: 'App for musicians',
+    imagen: '0',
+    url: 'http://okeytoplay.herokuapp.com/',
+  },
+  {
+    id: '2',
+    titulo: 'Aha! Moment',
+    description: 'Write your ideas',
+    imagen: '1',
+    url: 'https://aha-moment.netlify.com/',
+  },
+  {
+    id: '3',
+    titulo: 'Lucky',
+    description: 'Arcade game',
+    imagen: '2',
+    url: 'https://johanbautista.github.io/GAME/',
+  },
+  {
+    id: '4',
+    titulo: 'Appointments',
+    description: 'React practice',
+    imagen: '3',
+    url: 'https://jb-citas-veterinaria.netlify.com/',
+  },
+  {
+    id: '5',
+    titulo: 'Boletaire!',
+    description: 'Work hard , dream big',
+    imagen: '4',
+    url: 'https://www.youtube.com/watch?v=EsLn2nKQduc',
+  },
+  {
+    id: '6',
+    titulo: 'Etiquetas',
+    description: 'Performance. Meri Caballero',
+    imagen: '5',
+    url: 'https://www.youtube.com/watch?v=OBLu5CItsCQ',
+  },
+  {
+    id: '7',
+    titulo: 'Cafe Central',
+    description: 'Video Marketing',
+    imagen: '6',
+    url: 'https://www.youtube.com/watch?v=PQKWb62JsAg',
+  },
+  {
+    id: '8',
+    titulo: 'Web Responsive',
+    description: 'Certicated freeCodeCamp',
+    imagen: '7',
+    url: 'https://www.behance.net/gallery/94458879/Responsive-Web-freeCodeCamp',
+  },
+];
+console.log(info);
 
 const Projects = () => {
   return (
     <div className="container projects">
       <h3>Projects start</h3>
-
-      <CardColumns>
-        <a href="http://okeytoplay.herokuapp.com/" className="img__wrap">
-          <Card className="p-3 text-card">
-            <Card.Img variant="top" src={okey} className="figure" />
-            <div className="img__description_layer">
-              <div className="img__description">
-                <h2>ok to play</h2>
-                <p>App for Musicians</p>
-              </div>
+      {info.map((info, i) => {
+        return (
+          <div key={i}>
+            <div className="prueba">
+              {/* <a href={info.url}>{info.titulo}</a>
+              <p>{info.description}</p> */}
+                <a href={info.url} className="img__wrap">
+                  <Card className="p-3 text-card">
+                    <Card.Img variant="top" src={okey} className="figure" />
+                    <div className="img__description_layer">
+                      <div className="img__description">
+                        <h2>{info.titulo}</h2>
+                        <p>{info.description}</p>
+                        <p>{info.imagen}</p>
+                      </div>
+                    </div>
+                  </Card>
+                </a>
             </div>
-          </Card>
-        </a>
-
-        <a href="https://aha-moment.netlify.com/" className="img__wrap">
-          <Card className="text-card  ">
-            <Card.Img variant="top" src={aha} className="figure caja" />
-            <div className="img__description_layer">
-              <div className="img__description">
-                <h2>Aha! Moment</h2>
-                <p>Write your ideas</p>
-              </div>
-            </div>
-          </Card>
-        </a>
-
-        <a href="https://johanbautista.github.io/GAME/" className="img__wrap">
-          <Card className="text-card">
-            <Card.Img variant="top" src={lucky} className="img__wrap" />
-            <div className="img__description_layer">
-              <div className="img__description">
-                <h2>LUCKY</h2>
-                <p>Arcade Game</p>
-              </div>
-            </div>
-          </Card>
-        </a>
-
-        <a
-          href="https://jb-citas-veterinaria.netlify.com/"
-          className="img__wrap"
-        >
-          <Card className="text-card">
-            <Card.Img variant="top" src={cita} className="img__wrap" />
-            <div className="img__description_layer">
-              <div className="img__description">
-                <h2>Appointments</h2>
-                <p>React Practice</p>
-              </div>
-            </div>
-          </Card>
-        </a>
-
-        <a href="https://www.youtube.com/watch?v=EsLn2nKQduc">
-          <Card bg="dark" text="white" className="text-center p-3 img__wrap">
-            <Card.Title> Work hard, dream big</Card.Title>
-            <div className="img__description_layer">
-              <div className="img__description">
-                <h3>Boletaire!</h3>
-              </div>
-            </div>
-          </Card>
-        </a>
-
-        <a
-          href="https://www.youtube.com/watch?v=OBLu5CItsCQ"
-          className="img__wrap"
-        >
-          <Card className="text-card">
-            <Card.Img variant="top" src={etiqueta} />
-            <div className="img__description_layer">
-              <div className="img__description">
-                <h2>Etiquetas</h2>
-                <p>Performance. Meri Caballero</p>
-              </div>
-            </div>
-          </Card>
-        </a>
-
-        <a
-          href="https://www.youtube.com/watch?v=PQKWb62JsAg"
-          className="img__wrap"
-        >
-          <Card className="text-card">
-            <Card.Img variant="top" src={cafe} />
-            <div className="img__description_layer">
-              <div className="img__description">
-                <h2>Cafe Central</h2>
-                <p>Video Marketing</p>
-              </div>
-            </div>
-          </Card>
-        </a>
-
-        <a
-          href="https://www.behance.net/gallery/94458879/Responsive-Web-freeCodeCamp"
-          className="img__wrap"
-        >
-          <Card className="text-card">
-            <Card.Img variant="top" src={pt2} />
-            <div className="img__description_layer">
-              <div className="img__description">
-                <h2>Web Responsive</h2>
-                <p>Certicated freeCodeCamp</p>
-              </div>
-            </div>
-          </Card>
-        </a>
-
-        <a className="img__wrap">
-          <Card className="text-card">
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <div className="img__description_layer">
-              <div className="img__description">
-                <h2>Cafe Central</h2>
-                <p>Video Marketing</p>
-              </div>
-            </div>
-          </Card>
-        </a>
-
-        <Card className="p-3 text-card">
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Title>10</Card.Title>
-        </Card>
-
-        <Card className="text-card">
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Title>11</Card.Title>
-        </Card>
-
-        <Card bg="primary" text="white" className="text-center p-3 text-card">
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Title>12</Card.Title>
-        </Card>
-
-        <Card className="text-center text-card">
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Title>13</Card.Title>
-        </Card>
-
-        <Card className="text-card">
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Title>14</Card.Title>{' '}
-        </Card>
-
-        <Card className="text-right text-card ">
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Title>15</Card.Title>
-        </Card>
-
-        <Card className="text-card">
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Title>16</Card.Title>
-        </Card>
-      </CardColumns>
+          </div>
+        );
+      })}
     </div>
   );
 };
