d2023285d8d797b8fce7b64e6c73561bd9248eca
johan Bautista <jsbparra@gmail.com>
2020-05-29 10:59:17 +0200

    Updates

---
+++ precache-manifest.5eeea7fb6200f11ca1bcf0afc69958cb.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26cd41c60af05ec48631bd6308901ee8",
+    "url": "/jBautista_web/index.html"
+  },
+  {
+    "revision": "afb7f0c3b51b0795e2b6",
+    "url": "/jBautista_web/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "dd9f599345e046fab831",
+    "url": "/jBautista_web/static/css/main.2f54e132.chunk.css"
+  },
+  {
+    "revision": "afb7f0c3b51b0795e2b6",
+    "url": "/jBautista_web/static/js/2.d9f17de9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jBautista_web/static/js/2.d9f17de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd9f599345e046fab831",
+    "url": "/jBautista_web/static/js/main.bca1a7d8.chunk.js"
+  },
+  {
+    "revision": "131aeaf1f669c931ff4c",
+    "url": "/jBautista_web/static/js/runtime-main.2ec62fe4.js"
+  },
+  {
+    "revision": "1086a410b0d2bf23fb2755d4248d2ea2",
+    "url": "/jBautista_web/static/media/1css.1086a410.png"
+  },
+  {
+    "revision": "bed580cf9301a538e0de743dfd06477d",
+    "url": "/jBautista_web/static/media/2html.bed580cf.png"
+  },
+  {
+    "revision": "3a5c2a32d70af6be0430b261f696422e",
+    "url": "/jBautista_web/static/media/3bbdd.3a5c2a32.png"
+  },
+  {
+    "revision": "c1a9846ba67de088149dda9dd5ab49bf",
+    "url": "/jBautista_web/static/media/4git.c1a9846b.png"
+  },
+  {
+    "revision": "2a4e04bea33833de6efe58cce9b62e63",
+    "url": "/jBautista_web/static/media/5react.2a4e04be.png"
+  },
+  {
+    "revision": "2b65ae85bf4e2619d1495128c1a4392b",
+    "url": "/jBautista_web/static/media/about.2b65ae85.jpg"
+  },
+  {
+    "revision": "4571772cb7da69dcea492e1cf4738cd3",
+    "url": "/jBautista_web/static/media/aboutme.4571772c.jpg"
+  },
+  {
+    "revision": "bc232a930429893098c8940c89674e15",
+    "url": "/jBautista_web/static/media/ah-1.bc232a93.png"
+  },
+  {
+    "revision": "55280ff9075fcbb714588ac8acd15b2f",
+    "url": "/jBautista_web/static/media/ah-2.55280ff9.png"
+  },
+  {
+    "revision": "751da1e39276347ac6e14c919a5df07a",
+    "url": "/jBautista_web/static/media/ah-3.751da1e3.png"
+  },
+  {
+    "revision": "4d8c33ad2d08b12ad99de53f1a571bbb",
+    "url": "/jBautista_web/static/media/contact.4d8c33ad.jpg"
+  },
+  {
+    "revision": "65666a4b689b61629a878b70d4586877",
+    "url": "/jBautista_web/static/media/educa.65666a4b.png"
+  },
+  {
+    "revision": "40f4f8203817964ca7ef4ba967d879e5",
+    "url": "/jBautista_web/static/media/educa2.40f4f820.png"
+  },
+  {
+    "revision": "24c81c9952b6009e2902cb1a79492b90",
+    "url": "/jBautista_web/static/media/error.24c81c99.png"
+  },
+  {
+    "revision": "09bc7d21913b16fa08f90eb2a0d4e83e",
+    "url": "/jBautista_web/static/media/home.09bc7d21.jpg"
+  },
+  {
+    "revision": "3da0af2d59f9db5ba81777b6174a67ca",
+    "url": "/jBautista_web/static/media/languages.3da0af2d.jpg"
+  },
+  {
+    "revision": "aa1a036c9b6d2dfeb4ded16c5b03e48c",
+    "url": "/jBautista_web/static/media/lk-1.aa1a036c.png"
+  },
+  {
+    "revision": "781a88f4f7432f4832a32f49a2cba9de",
+    "url": "/jBautista_web/static/media/lk-2.781a88f4.png"
+  },
+  {
+    "revision": "6b4942d8e69f3fc936db05c2e9a06bf6",
+    "url": "/jBautista_web/static/media/lk-3.6b4942d8.png"
+  },
+  {
+    "revision": "da5c16fdc6276469a2deec1e47912f8d",
+    "url": "/jBautista_web/static/media/ok-1.da5c16fd.png"
+  },
+  {
+    "revision": "24b4ebdb88f44b7cb884470dff876707",
+    "url": "/jBautista_web/static/media/ok-2.24b4ebdb.png"
+  },
+  {
+    "revision": "fb48a6985eb66aace8168f8c2bae820a",
+    "url": "/jBautista_web/static/media/ok-3.fb48a698.png"
+  },
+  {
+    "revision": "fa5becaf93114348f13473909cf6377d",
+    "url": "/jBautista_web/static/media/whyme.fa5becaf.jpg"
+  }
+]);
